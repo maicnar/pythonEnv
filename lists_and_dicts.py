eb4ab4cb68f8962016b7e32fc98d0bf7a2718ea2
Miguel Hdz <mighdz93@gmail.com>
2021-10-06 01:25:20 -0500

    remove pandas

---
+++ lists_and_dicts.py
@@ -0,0 +1,11 @@
+def run():
+    my_list = [1, "Hello", True, 4.5]
+    my_dict = {"firstname": "Facundo","lastname": "García"}
+
+    super_list = [
+        {"firstname": "Facundo","lastname": "García"},
+        {"firstname": "Facundo","lastname": "García"},
+        {"firstname": "Facundo","lastname": "García"},
+        {"firstname": "Facundo","lastname": "García"},
+        {"firstname": "Facundo","lastname": "García"},
+    ]
