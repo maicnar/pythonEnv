1483872a663d01b60ab92a0485f497b52a2a71f7
Miguel Hdz <mighdz93@gmail.com>
2021-10-06 02:02:09 -0500

    adding some small changes

--- lists_and_dicts.py
+++ lists_and_dicts.py
@@ -4,8 +4,20 @@ def run():
 
     super_list = [
         {"firstname": "Facundo","lastname": "García"},
-        {"firstname": "Facundo","lastname": "García"},
-        {"firstname": "Facundo","lastname": "García"},
-        {"firstname": "Facundo","lastname": "García"},
-        {"firstname": "Facundo","lastname": "García"},
+        {"firstname": "Miguel","lastname": "Hernández"},
+        {"firstname": "Pepe","lastname": "Torres"},
+        {"firstname": "María","lastname": "López"},
+        {"firstname": "Sandra","lastname": "Castro"},
     ]
+
+    super_dict = {
+        "natural_nums": [1,2,3,4,5],
+        "integer_nums": [-1,-2,0,1,2],
+        "floating_nums": [1.1,4.5,6.43]
+    }
+
+    for key, value in super_dict.items():
+        print(key,"-",value)
+
+if __name__ == '__main__':
+    run()
